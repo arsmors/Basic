e17746382dc6154801a75555a3a0c08e2e2edc14
arsens <aa@aa.lv>
2020-05-08 00:43:48 +0300

    json test2 > create models

--- test/unit/location_test.dart
+++ test/unit/location_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:basic_test/models/location.dart';
 
 void main() {
   test("test location deserialization", () {
@@ -19,5 +20,14 @@ void main() {
     final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
 
     expect("Arashiyama Bamboo Grove", equals(locationMap['name']));
+
+    final location = Location.fromJson(locationMap);
+
+    expect(location.name, equals(locationMap['name']));
+    expect(location.url, equals(locationMap['url']));
+
+    expect(location.facts[0].title, equals(locationMap['facts'][0]['title']));
+    expect(location.facts[0].text, equals(locationMap['facts'][0]['text']));
+
   });
 }
